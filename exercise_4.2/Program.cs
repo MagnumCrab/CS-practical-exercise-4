608cf3b95675355ee1cc07477e78d6e803ba6e68
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-16 19:18:59 +0300

    add exercise_4.2

---
+++ exercise_4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите любое число");
+int i = Convert.ToInt32(Console.ReadLine());
+int number1 = 0;
+
+while (i > 0)
+{
+    int number2 = i % 10;
+    i = i / 10;
+    number1 = number1 + number2;
+}
+Console.WriteLine("Сумма цифр в числе равна: " + number1);
