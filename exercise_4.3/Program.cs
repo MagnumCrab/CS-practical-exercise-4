d6b0e79d29d1505ff0af4d6a377367eafc4b1bde
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-18 19:24:21 +0300

    add exercise_4.3

---
+++ exercise_4.3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+Console.WriteLine("Задайте количество элементов массива N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[N];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + numbers[i] + " ");
+}
